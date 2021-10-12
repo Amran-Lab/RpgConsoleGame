104ab32b58e7e0ac503a5a7dd46eea674a67160d
AmranLab <52491921+Amran-Lab@users.noreply.github.com>
2021-10-12 09:01:19 +0000

    print board

--- include/board.h
+++ include/board.h
@@ -6,6 +6,17 @@ class Board {
   std::vector<std::vector<std::string>> board;
   public:
     Board(){
+      std::vector<std::string> row = {" x "," x "," x "," x "," x "," x "," x "," x "," x "," x "};
+      board = {row,row,row,row,row,row,row,row,row,row};
+    }
+
+    void printBoard(){
+      for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+          printf("%s",board[i][j].c_str());
+        }
+        printf("\n");
+      }
     }
 
 
--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 
 using namespace std;
 
+
 int main() {
-	std::cout << "Hello World" << endl;
+  Board board;
+  board = Board();
+  board.printBoard();
 }
