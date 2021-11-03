4b273bf84f80afbc64278306a8c5a03bf2b52ae3
AmranLab <52491921+Amran-Lab@users.noreply.github.com>
2021-11-03 00:45:01 +0000

    More Comments

--- include/constants.h
+++ include/constants.h
@@ -1,11 +1,11 @@
 #include <vector>
 #ifndef GAMESTATE_H
 #define GAMESTATE_H
-
+// Game State
 enum State { Adventure, Menu, Stats, Items, Fighting, ChangeLevel, EndGame};
-
+// Level State
 enum Level { level_1 = 1, level_2 = 20, level_3 = 100, level_4 = 300, end_game = 100000};
-
+// Armour properties 
 struct Armour{
   std::string armourName;
   bool armour;
@@ -13,11 +13,11 @@ struct Armour{
   int cost;
   bool bought;
 };
-
+// Global used throughout code
 extern State gameState;
-
+// Golbal used throughout game
 extern Level level;
-
+// Constant and used in multiple place
 extern std::vector<Armour> shopItems;
 
 #endif
